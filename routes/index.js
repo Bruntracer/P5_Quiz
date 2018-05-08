3d879b88c0b436cf78c82aeb758ed4373140a49a
Bruntracer <bruno2000@live.com>
2018-05-08 20:07:59 +0200

    credits

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
+
+//const Sequelize = require('sequelize');
+//const sequelize = require("../models/index");
+
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'P5_Quiz' });
 });
 
-// Author page.
-router.get('/author', (req, res, next) => {
-    res.render('author');
+/* GET creditos. */
+router.get('/credits', function(req, res, next) {
+    res.render('credits', { title:'Quiz', name: 'Bruno Gonzalez Lopez' });
 });
+
+/*
+router.get('/quizzes', function(req, res, next) {
+    sequelize.models.quiz.findAll().
+    then( quizzes => {
+        res.render('quizzes', {quizzes});
+    })
+    .catch(Sequelize.ValidationError, error => {
+    })
+    .catch(error => {
+    });
+});
+
+
+*/
 
 module.exports = router;
